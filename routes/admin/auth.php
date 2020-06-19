dc7bbbcde03bad260e1c428de397b84c527f7bc1
ede <dnuez3204@gmail.com>
2020-06-19 00:06:29 -0500

    cambia rutas a minusculas

--- routes/admin/auth.php
+++ routes/admin/auth.php
@@ -1,8 +1,8 @@
 <?php
 // rutas sobre el modulo de Acceso
 $router->post('/auth/login','Auth\LoginController');
-$router->post('/auth/loginClient','Auth\LoginClientController');
+$router->post('/auth/loginclient','Auth\LoginClientController');
 $router->post('/auth/forgot','Auth\ForgotController');
 $router->post('/auth/change','Auth\ChangeController');
 // $router->post('/auth/GetNewCodePlatform','Auth\GetNewCodePlatformController');
-$router->post('/auth/VerifyCodePlatform','Auth\VerifyCodePlatformController');
+$router->post('/auth/verifycodeplatform','Auth\VerifyCodePlatformController');
